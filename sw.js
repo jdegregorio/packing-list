48e742054509f31dde998feab5a423f68f91cd44
Joseph DeGregorio <Joseph.DeGregorio@gmail.com>
2024-11-03 19:36:10 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,20 @@
-const CACHE_NAME = 'packing-list-cache-v1';
+const CACHE_NAME = 'packing-list-cache-v3';
 const urlsToCache = [
     '/',
     '/index.html',
     '/styles.css',
     '/script.js',
     '/manifest.json',
-    '/icons/icon-192.png',
-    '/icons/icon-512.png'
+    '/assets/icons/icon-192.png',
+    '/assets/icons/icon-512.png',
+    // Add Materialize CSS and JS URLs
+    'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
+    'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://code.jquery.com/jquery-3.6.0.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
+    // Add SortableJS
+    'https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js'
 ];
 
 self.addEventListener('install', function(event) {
